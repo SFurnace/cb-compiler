a1f28155dd61b2201f44877a9cdf5c0fa8100bd7
drcz <cz188b@foxmail.com>
2020-03-02 01:03:54 +0800

    lexer completed

--- private/lexer.rkt
+++ private/lexer.rkt
@@ -15,7 +15,7 @@
   (lexer
    ;; empty
    [(:+ (union blank (char-set "\r\n"))) ignored]
-   
+
    ;; keywords
    ["void" (VOID)]
    ["char" (CHAR)]
@@ -59,34 +59,79 @@
 
    ;; comment
    [(:: "//" (:* (:- any-char "\r" "\n"))) (COMMENT lexeme)]
-   ["/*" (read-comment input-port start-pos)]
-   ))
+   ["/*" (BLOCK-COMMENT input-port start-pos)]
+
+   ;; string
+   [#\" (STRING input-port start-pos)]
+
+   ;; char
+   [#\' (CHARACTER input-port start-pos)]))
 
 ;; Helper
-(define (make-position in)
-  (let-values ([(l c p) (port-next-location in)])
-    (position p l c)))
+(define (map-escape-char c)
+  (cdr (or (assv c '((#\\ . #\\)
+                     (#\' . #\')
+                     (#\" . #\")
+                     (#\newline . #\newline)
+                     (#\return . #\return)
+                     (#\n . #\newline)
+                     (#\r . #\return)
+                     (#\0 . #\null)
+                     (#\b . #\backspace)
+                     (#\t . #\tab)
+                     (#\v . #\vtab)))
+           (raise (format "bad escape char: ~s" c)))))
 
-(define (read-comment in start)
-  (define out (open-output-string))
-  (parameterize ([current-input-port in]
-                 [current-output-port out])
-    (let loop ([count 1])
-      (define s (peek-string 2 0))
-      (cond 
-        [(eof-object? s)
-         (when (> count 0)
-           (error 'read-comment "get eof-object"))]
-        [(string=? "*/" s)
-         (write-string (read-string 2))
-         (set! count (sub1 count))
-         (unless (= count 0)
-           (loop count))]
-        [(string=? "/*" s)
-         (write-string (read-string 2))
-         (loop (add1 count))]
-        [else
-         (write-char (read-char))
-         (loop count)])))
-  (Token 'COMMENT (string-append "/*" (get-output-string out))
-         start (make-position in)))
+;; Special Lexer
+(define-special-lex CHARACTER
+  (let ([c (read-char)])
+    (cond
+      [(eof-object? c) (raise "get eof")]
+      [(char=? c #\') (raise "empty character token")]
+      [(char=? c #\\) (write-char (map-escape-char (read-char)))]
+      [else (write-char c)]))
+  (unless (char=? (read-char) #\')
+    (raise "bad character token")))
+
+(define-special-lex STRING
+  (let loop ()
+    (define c (read-char))
+    (cond
+      [(eof-object? c) (raise "get eof")]
+      [(memv c '(#\newline #\return)) (raise "unexpected newline")]
+      [(char=? c #\")]
+      [(char=? c #\\)
+       (write-char
+        (cond
+          [(regexp-match-peek #px"^[0-7]{3}" (current-input-port))
+           =>
+           (λ (x)
+             (read-string 3)
+             (integer->char (string->number (bytes->string/utf-8 (car x)) 8)))]
+          [(regexp-match-peek #px"^\r\n" (current-input-port))
+           =>
+           (λ (x)
+             (read-string 2))]
+          [else (map-escape-char (read-char))]))
+       (loop)]
+      [else
+       (write-char c)
+       (loop)])))
+
+(define-special-lex BLOCK-COMMENT
+  (write-string "/*")
+  (let loop ([count 1])
+    (define s (peek-string 2 0))
+    (cond
+      [(eof-object? s) (when (> count 0) (raise "get eof"))]
+      [(string=? "*/" s)
+       (write-string (read-string 2))
+       (set! count (sub1 count))
+       (unless (= count 0)
+         (loop count))]
+      [(string=? "/*" s)
+       (write-string (read-string 2))
+       (loop (add1 count))]
+      [else
+       (write-char (read-char))
+       (loop count)])))
--- test/lexer-test.rkt
+++ test/lexer-test.rkt
@@ -1,14 +1,33 @@
 #lang racket/base
 (require "../private/lexer.rkt")
 
-(define str "
-00 a1 a2 a3
+(define str #<<STR
+00
+a1 a2 a3 _fds_ _FDS012 _0dfsA
+
+//
+//sdfa
+// fdsa //
 /* /*123 */ */ /*.*/
 /* //fdsa
   /* 100 */
 //jfdlsa/
 */
-")
+
+""
+"  adf  \\ a \t \0 \123 \\"
+"\"\
+\
+\""
+
+'a'
+'\''
+'"'
+'\"'
+'\n'
+STR
+  )
+
 (define in (open-input-string str))
 (define ts (do-lex lex in))
 
@@ -16,6 +35,6 @@
   (format "~a:~a:~a" (position-line p) (position-col p) (position-offset p)))
 
 (for ([v (send-generic ts ppeek 100)])
-  (fprintf (current-output-port) "~a(~a-~a): ~a\n"
+  (fprintf (current-output-port) "~a(~a-~a): ~s\n"
            (Token-name v) (pos->str (Token-start-pos v))
            (pos->str (Token-end-pos v)) (Token-value v)))
